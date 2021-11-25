239756a1ee11110f2e0b48d5a9d7894703bc3d5d
errampy <rampython2019@gmail.com>
2021-11-25 23:37:47 +0530

    Hello

--- myproject/testapp/views.py
+++ myproject/testapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>hello This Is My First Project..</h1>")
+    return HttpResponse("<h1>Hi hello This Is My First Project..</h1>")
